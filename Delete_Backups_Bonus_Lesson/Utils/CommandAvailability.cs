6beaf4646879fc9aa682582f3f761f0a110b63bf
ablock <ablock@G2C-17>
2024-07-25 20:35:55 -0600

    Add project files.

---
+++ Delete_Backups_Bonus_Lesson/Utils/CommandAvailability.cs
@@ -0,0 +1,17 @@
+﻿namespace Delete_Backups_Bonus_Lesson.Utils
+{
+    internal class CommandAvailability : IExternalCommandAvailability
+    {
+        public bool IsCommandAvailable(UIApplication applicationData, CategorySet selectedCategories)
+        {
+            bool result = false;
+            UIDocument activeDoc = applicationData.ActiveUIDocument;
+            if (activeDoc != null && activeDoc.Document != null)
+            {
+                result = true;
+            }
+
+            return result;
+        }
+    }
+}
